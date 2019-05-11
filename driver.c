2dc0a52231da015fbc5d3c734d02b2a37a257917
Dean Kevorkian <deankevorkian@gmail.com>
2019-05-11 15:40:09 +0300

    Print error messages when realizing access for writing requests @ the device

--- driver.c
+++ driver.c
@@ -6,18 +6,18 @@
  * Redistributable under the terms of the GNU GPL.
  */
 
+#include <linux/init.h>
 #include <linux/module.h>
 #include <linux/moduleparam.h>
-#include <linux/init.h>
 
+#include <linux/blkdev.h>
+#include <linux/errno.h> /* error codes */
+#include <linux/fs.h>    /* everything... */
+#include <linux/genhd.h>
+#include <linux/hdreg.h>
 #include <linux/kernel.h> /* printk() */
-#include <linux/fs.h>     /* everything... */
-#include <linux/errno.h>  /* error codes */
 #include <linux/types.h>  /* size_t */
 #include <linux/vmalloc.h>
-#include <linux/genhd.h>
-#include <linux/blkdev.h>
-#include <linux/hdreg.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
 static char *Version = "1.4";
@@ -44,52 +44,60 @@ static struct request_queue *Queue;
  * The internal representation of our device.
  */
 static struct sbd_device {
-	unsigned long size;
-	spinlock_t lock;
-	u8 *data;
-	struct gendisk *gd;
+  unsigned long size;
+  spinlock_t lock;
+  u8 *data;
+  struct gendisk *gd;
 } Device;
 
 /*
  * Handle an I/O request.
  */
 static void sbd_transfer(struct sbd_device *dev, sector_t sector,
-		unsigned long nsect, char *buffer, int write) {
-	unsigned long offset = sector * logical_block_size;
-	unsigned long nbytes = nsect * logical_block_size;
+                         unsigned long nsect, char *buffer, int write) {
+  unsigned long offset = sector * logical_block_size;
+  unsigned long nbytes = nsect * logical_block_size;
 
-	if ((offset + nbytes) > dev->size) {
-		printk (KERN_NOTICE "sbd: Beyond-end write (%ld %ld)\n", offset, nbytes);
-		return;
-	}
-	printk("CHRISTIANITY DEBUG: pid of current process is %d", task_pid_nr(current));
-	printk("CHRISTIANITY DEBUG: tgid of current process is %d", current->tgid);
-	printk("CHRISTIANITY DEBUG: command of current process is %s", current->comm);
-	if (write)
-		memcpy(dev->data + offset, buffer, nbytes);
-	else
-		memcpy(buffer, dev->data + offset, nbytes);
+  if ((offset + nbytes) > dev->size) {
+    printk(KERN_NOTICE "sbd: Beyond-end write (%ld %ld)\n", offset, nbytes);
+    return;
+  }
+  printk("CHRISTIANITY DEBUG: pid of current process is %d",
+         task_pid_nr(current));
+  printk("CHRISTIANITY DEBUG: tgid of current process is %d", current->tgid);
+  printk("CHRISTIANITY DEBUG: command of current process is %s", current->comm);
+  if (write)
+    memcpy(dev->data + offset, buffer, nbytes);
+  else
+    memcpy(buffer, dev->data + offset, nbytes);
 }
 
 static void sbd_request(struct request_queue *q) {
-	struct request *req;
+  struct request *req;
 
-	req = blk_fetch_request(q);
-	while (req != NULL) {
-		// blk_fs_request() was removed in 2.6.36 - many thanks to
-		// Christian Paro for the heads up and fix...
-		//if (!blk_fs_request(req)) {
-		if (req == NULL || blk_rq_is_passthrough(req)) {
-			printk (KERN_NOTICE "Skip non-CMD request\n");
-			__blk_end_request_all(req, -EIO);
-			continue;
-		}
-		sbd_transfer(&Device, blk_rq_pos(req), blk_rq_cur_sectors(req),
-			bio_data(req->bio), rq_data_dir(req));
-		if ( ! __blk_end_request_cur(req, 0) ) {
-			req = blk_fetch_request(q);
-		}
-	}
+  req = blk_fetch_request(q);
+  while (req != NULL) {
+    // blk_fs_request() was removed in 2.6.36 - many thanks to
+    // Christian Paro for the heads up and fix...
+    // if (!blk_fs_request(req)) {
+    if (req == NULL || blk_rq_is_passthrough(req)) {
+      printk(KERN_NOTICE "Skip non-CMD request\n");
+      __blk_end_request_all(req, -EIO);
+      continue;
+    }
+
+    // In case the received request was not bypassing the
+    // file system, and we want to handle it - check whether or not
+    // it is read or write.
+    if (rq_data_dir(req) == 1) {
+      printk("Hello my friend. Time to go home. :(");
+    }
+    sbd_transfer(&Device, blk_rq_pos(req), blk_rq_cur_sectors(req),
+                 bio_data(req->bio), rq_data_dir(req));
+    if (!__blk_end_request_cur(req, 0)) {
+      req = blk_fetch_request(q);
+    }
+  }
 }
 
 /*
@@ -97,82 +105,81 @@ static void sbd_request(struct request_queue *q) {
  * calls this. We need to implement getgeo, since we can't
  * use tools such as fdisk to partition the drive otherwise.
  */
-int sbd_getgeo(struct block_device * block_device, struct hd_geometry * geo) {
-	long size;
+int sbd_getgeo(struct block_device *block_device, struct hd_geometry *geo) {
+  long size;
 
-	/* We have no real geometry, of course, so make something up. */
-	size = Device.size * (logical_block_size / KERNEL_SECTOR_SIZE);
-	geo->cylinders = (size & ~0x3f) >> 6;
-	geo->heads = 4;
-	geo->sectors = 16;
-	geo->start = 0;
-	return 0;
+  /* We have no real geometry, of course, so make something up. */
+  size = Device.size * (logical_block_size / KERNEL_SECTOR_SIZE);
+  geo->cylinders = (size & ~0x3f) >> 6;
+  geo->heads = 4;
+  geo->sectors = 16;
+  geo->start = 0;
+  return 0;
 }
 
 /*
  * The device operations structure.
  */
-static struct block_device_operations sbd_ops = {
-		.owner  = THIS_MODULE,
-		.getgeo = sbd_getgeo
-};
+static struct block_device_operations sbd_ops = {.owner = THIS_MODULE,
+                                                 .getgeo = sbd_getgeo};
 
 static int __init sbd_init(void) {
-	/*
-	 * Set up our internal device.
-	 */
-	Device.size = nsectors * logical_block_size;
-	spin_lock_init(&Device.lock);
-	Device.data = vmalloc(Device.size);
-	if (Device.data == NULL)
-		return -ENOMEM;
-	/*
-	 * Get a request queue.
-	 */
-	Queue = blk_init_queue(sbd_request, &Device.lock);
-	if (Queue == NULL)
-		goto out;
-	blk_queue_logical_block_size(Queue, logical_block_size);
-	/*
-	 * Get registered.
-	 */
-	major_num = register_blkdev(major_num, "sbd");
-	printk("WHO GOT THIS BIG DICK ENERGY?? WE DO!! WOOOO!! OUR MAJOR_NUM ISSSSS %d", &major_num);
-	if (major_num < 0) {
-		printk(KERN_WARNING "sbd: unable to get major number\n");
-		goto out;
-	}
-	/*
-	 * And the gendisk structure.
-	 */
-	Device.gd = alloc_disk(16);
-	if (!Device.gd)
-		goto out_unregister;
-	Device.gd->major = major_num;
-	Device.gd->first_minor = 0;
-	Device.gd->fops = &sbd_ops;
-	Device.gd->private_data = &Device;
-	strcpy(Device.gd->disk_name, "sbd0");
-	set_capacity(Device.gd, nsectors);
-	Device.gd->queue = Queue;
-	add_disk(Device.gd);
+  /*
+   * Set up our internal device.
+   */
+  Device.size = nsectors * logical_block_size;
+  spin_lock_init(&Device.lock);
+  Device.data = vmalloc(Device.size);
+  if (Device.data == NULL)
+    return -ENOMEM;
+  /*
+   * Get a request queue.
+   */
+  Queue = blk_init_queue(sbd_request, &Device.lock);
+  if (Queue == NULL)
+    goto out;
+  blk_queue_logical_block_size(Queue, logical_block_size);
+  /*
+   * Get registered.
+   */
+  major_num = register_blkdev(major_num, "sbd");
+  printk(
+      "WHO GOT THIS BIG DICK ENERGY?? WE DO!! WOOOO!! OUR MAJOR_NUM ISSSSS %d",
+      &major_num);
+  if (major_num < 0) {
+    printk(KERN_WARNING "sbd: unable to get major number\n");
+    goto out;
+  }
+  /*
+   * And the gendisk structure.
+   */
+  Device.gd = alloc_disk(16);
+  if (!Device.gd)
+    goto out_unregister;
+  Device.gd->major = major_num;
+  Device.gd->first_minor = 0;
+  Device.gd->fops = &sbd_ops;
+  Device.gd->private_data = &Device;
+  strcpy(Device.gd->disk_name, "sbd0");
+  set_capacity(Device.gd, nsectors);
+  Device.gd->queue = Queue;
+  add_disk(Device.gd);
 
-	return 0;
+  return 0;
 
 out_unregister:
-	unregister_blkdev(major_num, "sbd");
+  unregister_blkdev(major_num, "sbd");
 out:
-	vfree(Device.data);
-	return -ENOMEM;
+  vfree(Device.data);
+  return -ENOMEM;
 }
 
-static void __exit sbd_exit(void)
-{
-	del_gendisk(Device.gd);
-	put_disk(Device.gd);
-	unregister_blkdev(major_num, "sbd");
-	blk_cleanup_queue(Queue);
-	vfree(Device.data);
+static void __exit sbd_exit(void) {
+  del_gendisk(Device.gd);
+  put_disk(Device.gd);
+  unregister_blkdev(major_num, "sbd");
+  blk_cleanup_queue(Queue);
+  vfree(Device.data);
 }
 
 module_init(sbd_init);
